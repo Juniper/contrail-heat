77fbb1cf82a89dfc43a22aaa5b5daf08a21541c0
Jacek Swiderski <jacek.swiderski@codilime.com>
2014-10-15 15:34:18 +0200

    Make service_instance flake8 compatible
    Change-Id: I62dabcc5c76359878f609a6040adcbfd17e8c5e6

--- contrail_heat/resources/service_instance.py
+++ contrail_heat/resources/service_instance.py
@@ -1,11 +1,12 @@
 from heat.engine import constraints
 from heat.engine import properties
 from heat.openstack.common import log as logging
-from vnc_api.vnc_api import *
+from vnc_api import vnc_api
 from contrail_heat.resources.contrail import ContrailResource
 import uuid
 
 logger = logging.getLogger(__name__)
+
 
 class HeatServiceInstance(ContrailResource):
     PROPERTIES = (
@@ -13,7 +14,7 @@ class HeatServiceInstance(ContrailResource):
         INTERFACE_LIST, SCALE_OUT, HA_MODE
     ) = (
         'name', 'service_template', 'auto_policy', 'availability_zone',
-	'interface_list', 'scale_out', 'ha_mode'
+        'interface_list', 'scale_out', 'ha_mode'
     )
 
     _INTERFACE_LIST_KEYS = (
@@ -60,7 +61,8 @@ class HeatServiceInstance(ContrailResource):
         ),
         INTERFACE_LIST: properties.Schema(
             properties.Schema.LIST,
-            _('An ordered list of interfaces to be added to this service instance'),
+            _('An ordered list of interfaces to be added to this '
+              'service instance'),
             schema=properties.Schema(
                 properties.Schema.MAP,
                 schema={
@@ -73,25 +75,26 @@ class HeatServiceInstance(ContrailResource):
                         _('IP for this interface')
                     ),
                     STATIC_ROUTES: properties.Schema(
-                    	properties.Schema.LIST,
-            		_('An ordered list of static routes to be added to this interface'),
-			schema=properties.Schema(
-				properties.Schema.MAP,
-				schema={
-					PREFIX: properties.Schema(
-						properties.Schema.STRING,
-						_('Route prefix'),
-					),
-					NEXT_HOP: properties.Schema(
-						properties.Schema.STRING,
-						_('Nexthop'),
-					),
-					NEXT_HOP_TYPE: properties.Schema(
-						properties.Schema.STRING,
-						_('Nexthop Type'),
-					)
-				}
-			)
+                        properties.Schema.LIST,
+                        _('An ordered list of static routes to be added '
+                          'to this interface'),
+                        schema=properties.Schema(
+                            properties.Schema.MAP,
+                            schema={
+                                PREFIX: properties.Schema(
+                                    properties.Schema.STRING,
+                                    _('Route prefix'),
+                                ),
+                                NEXT_HOP: properties.Schema(
+                                    properties.Schema.STRING,
+                                    _('Nexthop'),
+                                ),
+                                NEXT_HOP_TYPE: properties.Schema(
+                                    properties.Schema.STRING,
+                                    _('Nexthop Type'),
+                                )
+                            }
+                        )
                     ),
                 }
             ),
@@ -102,26 +105,26 @@ class HeatServiceInstance(ContrailResource):
             _('Scale out property'),
             update_allowed=False,
             schema={
-                    MAX_INSTANCES: properties.Schema(
-                        properties.Schema.INTEGER,
-                        _('Number of instances of service instance'),
-            		default=1,
-                    ),
-                    AUTO_SCALE: properties.Schema(
-                        properties.Schema.BOOLEAN,
-                        _('Whether to auto scale the service instance'),
-            		default=False,
-                    )
+                MAX_INSTANCES: properties.Schema(
+                    properties.Schema.INTEGER,
+                    _('Number of instances of service instance'),
+                    default=1,
+                ),
+                AUTO_SCALE: properties.Schema(
+                    properties.Schema.BOOLEAN,
+                    _('Whether to auto scale the service instance'),
+                    default=False,
+                )
             }
         ),
 
-	HA_MODE: properties.Schema(
-	    properties.Schema.STRING,
-	    _('High availability mode'),
-	    constraints=[
-	        constraints.AllowedValues(['active-active', 'active-standby']),
-	    ],
-	),
+        HA_MODE: properties.Schema(
+            properties.Schema.STRING,
+            _('High availability mode'),
+            constraints=[
+                constraints.AllowedValues(['active-active', 'active-standby']),
+            ],
+        ),
     }
 
     attributes_schema = {
@@ -130,7 +133,8 @@ class HeatServiceInstance(ContrailResource):
         "status": _("Status of the Service Instance."),
         "service_template": _("Service Template of the Service Instance."),
         "virtual_machines": _("Service VMs for the Service Instance."),
-        "active_service_vms": _("Number of service VMs active for this Service Instance."),
+        "active_service_vms": _("Number of service VMs active for this "
+                                "Service Instance."),
         "tenant_id": _("Tenant id of the Service Instance."),
         "show": _("All attributes."),
     }
@@ -139,38 +143,44 @@ class HeatServiceInstance(ContrailResource):
 
     def handle_create(self):
         try:
-            st_obj = self.vnc_lib().service_template_read(id=self.properties[self.SERVICE_TEMPLATE])
-        except NoIdError:
-            st_obj = self.vnc_lib().service_template_read(name=self.properties[self.SERVICE_TEMPLATE])
+            st_obj = self.vnc_lib().service_template_read(
+                id=self.properties[self.SERVICE_TEMPLATE])
+        except vnc_api.NoIdError:
+            st_obj = self.vnc_lib().service_template_read(
+                name=self.properties[self.SERVICE_TEMPLATE])
 
         tenant_id = self.stack.context.tenant_id
         project_obj = self.vnc_lib().project_read(id=str(uuid.UUID(tenant_id)))
-        si_obj = ServiceInstance(name=self.properties[self.NAME], parent_obj=project_obj)
+        si_obj = vnc_api.ServiceInstance(
+            name=self.properties[self.NAME], parent_obj=project_obj)
         si_uuid = self.vnc_lib().service_instance_create(si_obj)
 
-        si_prop = ServiceInstanceType()
+        si_prop = vnc_api.ServiceInstanceType()
 
         for intf in self.properties[self.INTERFACE_LIST]:
             if intf[self.VIRTUAL_NETWORK] == "auto":
                 vn = ""
             else:
                 vn = intf[self.VIRTUAL_NETWORK]
-            if_type = ServiceInstanceInterfaceType(virtual_network=vn)
+            if_type = vnc_api.ServiceInstanceInterfaceType(virtual_network=vn)
             si_prop.add_interface_list(if_type)
 
-        mgmt_network = self.properties[self.INTERFACE_LIST][0][self.VIRTUAL_NETWORK]
+        mgmt_network = self.properties[
+            self.INTERFACE_LIST][0][self.VIRTUAL_NETWORK]
         if mgmt_network != "auto":
             fq_name = self.vnc_lib().id_to_fq_name(mgmt_network)
             fq_name_str = ":".join(fq_name)
             si_prop.set_management_virtual_network(fq_name_str)
 
-        left_network = self.properties[self.INTERFACE_LIST][1][self.VIRTUAL_NETWORK]
+        left_network = self.properties[
+            self.INTERFACE_LIST][1][self.VIRTUAL_NETWORK]
         if left_network != "auto":
             fq_name = self.vnc_lib().id_to_fq_name(left_network)
             fq_name_str = ":".join(fq_name)
             si_prop.set_left_virtual_network(fq_name_str)
 
-        right_network = self.properties[self.INTERFACE_LIST][2][self.VIRTUAL_NETWORK]
+        right_network = self.properties[
+            self.INTERFACE_LIST][2][self.VIRTUAL_NETWORK]
         if right_network != "auto":
             fq_name = self.vnc_lib().id_to_fq_name(right_network)
             fq_name_str = ":".join(fq_name)
@@ -182,8 +192,8 @@ class HeatServiceInstance(ContrailResource):
         else:
             max_instances = self.properties[self.SCALE_OUT][self.MAX_INSTANCES]
             auto_scale = self.properties[self.SCALE_OUT][self.AUTO_SCALE]
-        scale_out = ServiceScaleOutType(max_instances=max_instances,
-            auto_scale=auto_scale)
+        scale_out = vnc_api.ServiceScaleOutType(max_instances=max_instances,
+                                                auto_scale=auto_scale)
         si_prop.set_scale_out(scale_out)
 
         si_prop.set_ha_mode(self.properties[self.HA_MODE])
@@ -199,7 +209,7 @@ class HeatServiceInstance(ContrailResource):
         si_obj = self.vnc_lib().service_instance_read(id=self.resource_id)
         dict = {}
         dict['name'] = si_obj.get_display_name()
-        dict['fq_name'] =si_obj.get_fq_name_str()
+        dict['fq_name'] = si_obj.get_fq_name_str()
         dict['tenant_id'] = si_obj.parent_uuid
         status = 'INACTIVE'
         dict['virtual_machines'] = []
@@ -225,18 +235,20 @@ class HeatServiceInstance(ContrailResource):
             status = "ACTIVE"
         dict['status'] = status
         dict['active_service_vms'] = active_count
-        dict['service_template'] = ':'.join(si_obj.get_service_template_refs()[0]['to'])
-	return dict
+        dict['service_template'] = ':'.join(
+            si_obj.get_service_template_refs()[0]['to'])
+        return dict
 
     def handle_delete(self):
         try:
             self.vnc_lib().service_instance_delete(id=self.resource_id)
-        except:
-            pass 
+        except Exception:
+            pass
 
     def handle_update(self, json_snippet, tmpl_diff, prop_diff):
         # TODO
-	pass
+        pass
+
 
 def resource_mapping():
     return {
