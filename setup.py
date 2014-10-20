2d042778815acf1f710481ee4e4916e26b0537bd
Praneet Bachheti <praneetb@juniper.net>
2014-10-20 10:51:21 -0700

    modify setup.py to put heat resources, templates and env files in /usr/lib/heat/ heat server will pick up the resources from this /usr/lib/heat/ directory
    Change-Id: I938437573323a7a4365d744052bcd501120b4164

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@
 #
 
 from setuptools import setup, find_packages
+import glob, os
+
+def get_files(source, target):
+    files = filter(lambda x: '__init__.py' not in x, glob.glob(source+'/*'))
+    return (target, files)
 
 def requirements(filename):
     with open(filename) as f:
@@ -18,4 +23,7 @@ setup(
     long_description="Contrail heat resources and templates",
 
     install_requires=requirements('requirements.txt'),
+    data_files=[get_files('contrail_heat/resources', '/usr/lib/heat/resources'),
+                get_files('contrail_heat/template', '/usr/lib/heat/template'),
+                get_files('contrail_heat/env', '/usr/lib/heat/env')]
 )
