adeb874204957a193bf33bd0d1b149f057e67992
Praneet Bachheti <praneetb@juniper.net>
2015-01-26 18:21:35 -0800

    - allow shared and external attribute in virtual_network - let user update the virtual network using heat api
    Change-Id: I7c8975f9bd6a7b772ade5b2fd6e0e84e099bec53

--- contrail_heat/resources/virtual_network.py
+++ contrail_heat/resources/virtual_network.py
@@ -1,6 +1,7 @@
 import uuid
 
 from contrail_heat.resources import contrail
+from heat.engine import constraints
 from heat.engine import properties
 from heat.openstack.common import log as logging
 from vnc_api import vnc_api
@@ -10,21 +11,42 @@ LOG = logging.getLogger(__name__)
 
 class ContrailVirtualNetwork(contrail.ContrailResource):
     PROPERTIES = (
-        NAME, ROUTE_TARGETS
+        NAME, ROUTE_TARGETS, SHARED, EXTERNAL
     ) = (
-        'name', 'route_targets'
+        'name', 'route_targets', 'shared', 'external'
     )
 
     properties_schema = {
         NAME: properties.Schema(
             properties.Schema.STRING,
             _('Virtual Network name.'),
-            update_allowed=False,
+            update_allowed=True,
         ),
         ROUTE_TARGETS: properties.Schema(
             properties.Schema.LIST,
             _('Route Targets list.'),
             default=[],
+            update_allowed=True,
+        ),
+        SHARED: properties.Schema(
+            properties.Schema.STRING,
+            _('Whether this network should be shared across all tenants. '
+              'Note that the default policy setting restricts usage of this '
+              'attribute to administrative users only.'),
+            default='False',
+            constraints=[
+                constraints.AllowedValues(['True', 'False']),
+            ],
+            update_allowed=True
+        ),
+        EXTERNAL: properties.Schema(
+            properties.Schema.STRING,
+            _('Whether this network should be external.'),
+            default='False',
+            constraints=[
+                constraints.AllowedValues(['True', 'False']),
+            ],
+            update_allowed=True
         ),
     }
 
@@ -32,8 +54,12 @@ class ContrailVirtualNetwork(contrail.ContrailResource):
         "name": _("The name of the Virtual Network."),
         "fq_name": _("The FQ name of the Virtual Network."),
         "route_targets": _("Route Targets list."),
+        "shared": _("shared across all tenants."),
+        "external": _("external."),
         "show": _("All attributes."),
     }
+
+    update_allowed_keys = ('Properties',)
 
     def handle_create(self):
         tenant_id = self.stack.context.tenant_id
@@ -46,8 +72,41 @@ class ContrailVirtualNetwork(contrail.ContrailResource):
         vn_obj.set_route_target_list(vnc_api.RouteTargetList(
             ["target:" + route for route in self.properties[
                 self.ROUTE_TARGETS]]))
+        if self.properties[self.SHARED] == "True":
+            vn_obj.set_is_shared(True)
+        else:
+            vn_obj.set_is_shared(False)
+        if self.properties[self.EXTERNAL] == "True":
+            vn_obj.set_router_external(True)
+        else:
+            vn_obj.set_router_external(False)
         vn_uuid = self.vnc_lib().virtual_network_create(vn_obj)
         self.resource_id_set(vn_uuid)
+
+    def handle_update(self, json_snippet, tmpl_diff, prop_diff):
+        tenant_id = self.stack.context.tenant_id
+        project_obj = self.vnc_lib().project_read(id=str(uuid.UUID(tenant_id)))
+
+        try:
+            vn_obj = self.vnc_lib().virtual_network_read(id=self.resource_id)
+        except Exception as ex:
+            LOG.warn(_("Virtual Network %s not found.") % self.name)
+            raise ex
+
+        props = self.prepare_update_properties(json_snippet)
+
+        vn_obj.set_route_target_list(vnc_api.RouteTargetList(
+            ["target:" + route for route in props[
+                self.ROUTE_TARGETS]]))
+        if props[self.SHARED] == "True":
+            vn_obj.set_is_shared(True)
+        else:
+            vn_obj.set_is_shared(False)
+        if props[self.EXTERNAL] == "True":
+            vn_obj.set_router_external(True)
+        else:
+            vn_obj.set_router_external(False)
+        self.vnc_lib().virtual_network_update(vn_obj)
 
     def _show_resource(self):
         vn_obj = self.vnc_lib().virtual_network_read(id=self.resource_id)
@@ -58,6 +117,8 @@ class ContrailVirtualNetwork(contrail.ContrailResource):
             'route_targets': [
                 (rt[7:] if rt.startswith('target:') else rt) for rt in rts
             ],
+            'shared': vn_obj.get_is_shared(),
+            'external': vn_obj.get_router_external(),
         }
         return attrs
 
