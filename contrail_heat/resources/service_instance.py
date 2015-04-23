e85e93a3bbae3b36f93dc637f84a4311792eb05f
Praneet Bachheti <praneetb@juniper.net>
2015-04-23 10:55:09 -0700

    heat support for static-routes for service chain added heat templates and env files for static routes example.
    Closes-bug: 1513288, 1541633
    Change-Id: I47d73843a55dcf297469b6465b43d96aaa2f2d19

--- contrail_heat/resources/service_instance.py
+++ contrail_heat/resources/service_instance.py
@@ -71,15 +71,18 @@ class HeatServiceInstance(ContrailResource):
                     VIRTUAL_NETWORK: properties.Schema(
                         properties.Schema.STRING,
                         _('Virtual Network for interface'),
+			update_allowed=True
                     ),
                     IP_ADDRESS: properties.Schema(
                         properties.Schema.STRING,
-                        _('IP for this interface')
+                        _('IP for this interface'),
+			update_allowed=True
                     ),
                     STATIC_ROUTES: properties.Schema(
                         properties.Schema.LIST,
                         _('An ordered list of static routes to be added '
                           'to this interface'),
+			update_allowed=True,
                         schema=properties.Schema(
                             properties.Schema.MAP,
                             schema={
@@ -100,7 +103,7 @@ class HeatServiceInstance(ContrailResource):
                     ),
                 }
             ),
-            update_allowed=False
+            update_allowed=True
         ),
         SCALE_OUT: properties.Schema(
             properties.Schema.MAP,
@@ -110,11 +113,13 @@ class HeatServiceInstance(ContrailResource):
                 MAX_INSTANCES: properties.Schema(
                     properties.Schema.INTEGER,
                     _('Number of instances of service instance'),
+		    update_allowed=True,
                     default=1,
                 ),
                 AUTO_SCALE: properties.Schema(
                     properties.Schema.BOOLEAN,
                     _('Whether to auto scale the service instance'),
+		    update_allowed=True,
                     default=False,
                 )
             }
@@ -161,6 +166,7 @@ class HeatServiceInstance(ContrailResource):
         if len(svc_tmpl_if_list) != len(svc_inst_if_list):
             raise vnc_api.BadRequest
 
+        if_index = 0
         si_prop = vnc_api.ServiceInstanceType()
         for intf in self.properties[self.INTERFACE_LIST]:
             virt_net = intf[self.VIRTUAL_NETWORK]
@@ -171,8 +177,17 @@ class HeatServiceInstance(ContrailResource):
                 vn_name = ":".join(fq_name)
             else:
                 vn_name = virt_net
-            if_type = vnc_api.ServiceInstanceInterfaceType(virtual_network=vn_name)
+
+            # now check for static routes on this interface
+            routes_list = {}
+            if svc_tmpl_if_list[if_index].get_static_route_enable(
+                ) and self.STATIC_ROUTES in intf:
+                routes_list['route'] = intf[self.STATIC_ROUTES]
+
+            if_type = vnc_api.ServiceInstanceInterfaceType(
+                virtual_network=vn_name,static_routes=routes_list or None)
             si_prop.add_interface_list(if_type)
+            if_index = if_index + 1
 
         if self.properties[self.SCALE_OUT] is None:
             max_instances = 1
@@ -197,6 +212,13 @@ class HeatServiceInstance(ContrailResource):
 
     def handle_update(self, json_snippet, tmpl_diff, prop_diff):
         try:
+            st_obj = self.vnc_lib().service_template_read(
+                id=self.properties[self.SERVICE_TEMPLATE])
+        except vnc_api.NoIdError:
+            st_obj = self.vnc_lib().service_template_read(
+                fq_name_str=self.properties[self.SERVICE_TEMPLATE])
+
+        try:
             si_obj = self.vnc_lib().service_instance_read(id=self.resource_id)
         except vnc_api.NoIdError:
             LOG.warn(_("Service Instance %s not found.") % self.name)
@@ -207,8 +229,40 @@ class HeatServiceInstance(ContrailResource):
 
         si_prop = si_obj.get_service_instance_properties()
 
+        svc_tmpl_if_list = st_obj.get_service_template_properties().interface_type
+        svc_inst_if_list = prop_diff.get(self.INTERFACE_LIST)
+        if len(svc_tmpl_if_list) != len(svc_inst_if_list):
+            raise vnc_api.BadRequest
+
+        if_index = 0
+        if prop_diff.get(self.INTERFACE_LIST):
+            si_prop.set_interface_list([])
+        for intf in prop_diff.get(self.INTERFACE_LIST):
+            virt_net = intf[self.VIRTUAL_NETWORK]
+            if virt_net == "auto":
+                vn_name = ""
+            elif not ":" in virt_net:
+                fq_name = self.vnc_lib().id_to_fq_name(virt_net)
+                vn_name = ":".join(fq_name)
+            else:
+                vn_name = virt_net
+
+            # now check for static routes on this interface
+            routes_list = {}
+            if svc_tmpl_if_list[if_index].get_static_route_enable(
+                ) and self.STATIC_ROUTES in intf:
+                routes_list['route'] = intf[self.STATIC_ROUTES]
+
+            if_type = vnc_api.ServiceInstanceInterfaceType(
+                virtual_network=vn_name,static_routes=routes_list or None)
+            si_prop.add_interface_list(if_type)
+            if_index = if_index + 1
+
         scaleprop = prop_diff.get(self.SCALE_OUT)
-        if scaleprop:
+        if not scaleprop:
+            max_instances = 1
+            auto_scale = False
+        else:
             max_instances = scaleprop.get(self.MAX_INSTANCES)
             auto_scale = scaleprop.get(self.AUTO_SCALE)
         scale_out = vnc_api.ServiceScaleOutType(max_instances=max_instances,
@@ -256,6 +310,8 @@ class HeatServiceInstance(ContrailResource):
                 break
 
     def handle_delete(self):
+        if not self.resource_id:
+            return
         try:
             # get the servers list
             si_obj = self.vnc_lib().service_instance_read(id=self.resource_id)
